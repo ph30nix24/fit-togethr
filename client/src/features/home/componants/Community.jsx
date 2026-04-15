e0b4ead538c6fafcf3b247221b96208a05a6dccd
phe0nix <starlordindia1230@gmail.com>
2026-04-15 07:48:39 +0530

    minor ui fix

--- client/src/features/home/componants/Community.jsx
+++ client/src/features/home/componants/Community.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowRight, FaComment } from "react-icons/fa6";
 import { IoFitnessOutline } from "react-icons/io5";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { FaChevronDown } from "react-icons/fa";
+import { FcLike } from "react-icons/fc";
 import { routines } from "../../../utils";
+import { CgGym } from "react-icons/cg";
+
+
 const Community = () => {
   return (
     <section className="w-full h-[110vh] relative z-1 px-5 lg:px-40 lg:py-10 bg-linear-to-tr to-[#FBF5F1] from-[#f89d6f]/30  overflow-hidden">
@@ -17,8 +21,8 @@ const Community = () => {
           <p className="mb-[3px] text-primary">&#8226;</p>
         </p>
       </div>
-      <div className="w-full h-full flex gap-5">
-        <div className="w-6/10 h-full">
+      <div className="w-full h-full flex gap-5 mb-1">
+        <div className="w-7/10 h-full">
           <h1 className="text-[8vw] lg:text-3xl font-secondary text-[#222] text-start mt-5 lg:mt-8 font-semibold hidden lg:block">
             Track, Discover & Share{" "}
             <span className="text-primary">Workouts</span>
@@ -90,7 +94,7 @@ const Community = () => {
               {routines.map((routine) => (
                 <div
                   key={routine.id}
-                  className="w-[32%] h-[48%] shrink-0 border border-gray-300 rounded-lg p-2"
+                  className="w-[32%] h-[48%] shrink-0 border border-gray-300 rounded-lg p-2 bg-white/70 shadow-md"
                 >
                   <div className="flex justify-between gap-2">
                     <img
@@ -109,6 +113,43 @@ const Community = () => {
                       </div>
                       <div className="text-xs font-body font">{routine.routine_name}</div>
                     </div>
+                  </div>
+                  <div className="w-full py-1 px-2 border border-black/20 rounded-lg mt-2 font-body text-[#333] bg-white/80">
+                    <div className="flex items-center justify-between">
+                      <p className="text-sm">{routine.category}</p>
+                      <CgGym className="text-lg" />
+                    </div>
+                    <hr className="border-black/20" />
+                    <div>
+                      {routine.exercises.map((exercise, index) => (
+                        <div
+                          key={index}
+                          className="text-sm font-body font flex w-full gap-1 justify-between mt-1"
+                        >
+                          <p>{exercise.name}</p>
+                          <div className="flex items-center text-xs ">
+                            <p className="mr-[2px]">&#8226;</p>
+                            <p>{exercise.sets} sets</p>
+                            <p>x {exercise.reps} {exercise.unit}</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="w-full flex items-center justify-between mt-2 font-body text-[#333]">
+                    <div className="flex items-center gap-2 ">
+                      <button className="text-sm font-body font flex items-center gap-1 px-3 py-1 border border-black/10 shadow bg-white/80 rounded-full pointer">
+                        <FcLike />
+                        <span className="text-xs">{routine.likes}</span>
+                      </button>
+                      <button className="text-sm font-body font flex items-center gap-1 px-3 py-1 border border-gray-600 rounded-full pointer hover:bg-white/80">
+                        <FaComment className="size-3" />
+                        <span className="text-xs">{routine.comments}</span>
+                      </button>
+                    </div>
+                    <button className="text-xs font-body font flex items-center gap-1 px-3 py-1.5 border border-black/10 shadow bg-white/80 rounded-full pointer">
+                      {routine.action_label}
+                    </button>
                   </div>
                 </div>
               ))}
